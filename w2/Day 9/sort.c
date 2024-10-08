bb655d86923210d55932300c891438658b662e06
mariapetrova.pmp <mariapetrova.pmp@gmail.com>
2024-10-08 23:09:54 +0300

    sort

---
+++ w2/Day 9/sort.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int input(int *);
+void sort(int *);
+void output(int *, int);
+
+int main()
+{
+    int a[SIZE], error;
+    char c;
+
+    error = input(a);
+    if (!error)
+    {
+        sort(a);
+    }
+    output(a, error);
+    return 0;
+}
+
+int input(int *a)
+{
+    int error = 0;
+    char c;
+    for (int i = 0; i < SIZE && !error; i++)
+    {
+        if (scanf("%d%c", &(a[i]), &c) != 2)
+        {
+            error = 1;
+            printf("ERROR");
+        }
+        else if (i == SIZE - 1 && c != '\n')
+        {
+            error = 1;
+            printf("ERROR 2");
+        }
+        else if (i < SIZE - 1 && c != ' ')
+        {
+            error = 1;
+            printf("ERROR 3");
+        }
+    }
+
+    return error;
+}
+
+void sort(int *a)
+{
+    int temp;
+    for (int i = 0; i < SIZE - 1; i++)
+    {
+        for (int j = 0; j < SIZE - 1 - i; j++)
+        {
+            if (a[j + 1] < a[j])
+            {
+                temp = a[j + 1];
+                a[j + 1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+
+void output(int *a, int error)
+{
+    if (!error)
+    {
+        for (int i = 0; i < SIZE - 1; i++, a++)
+        {
+            printf("%d ", *a);
+        }
+        printf("%d", *a);
+    }
+    else
+    {
+        printf("n/a");
+    }
+}
